7d54fac0e275e8500992f00618311af964a2575c
mjarkk <mkopenga@gmail.com>
2021-08-23 13:22:46 +0200

    proxy to backend in nextjs server

---
+++ dashboard/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `http://localhost:4000/api/:path*`,
+            },
+        ]
+    }
+}
