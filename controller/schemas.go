1ca3b245b5b314795b6912897768a1cf6c106e96
mjarkk <mkopenga@gmail.com>
2021-08-27 09:42:42 +0200

    add information about what the schemas file contains

--- controller/schemas.go
+++ controller/schemas.go
@@ -1,11 +1,14 @@
 package controller
 
+// This file contains json schemas for certain types
+// These are used by the dashboard to to validate user input data
+//
+// To be specific the monaco editor (the code editor we use in the dashboard) uses json schemas to validate the user input
+
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/script-development/RT-CV/helpers/schema"
 )
-
-// FIXME add tests for this route
 
 func routeGetCvSchema(c *fiber.Ctx) error {
 	resSchema, err := schema.From(RouteScraperScanCVBody{}, "/api/v1/schema/cv")
