c7dad34c79df322fc4c9b11973a3329fe8897be3
mjarkk <mkopenga@gmail.com>
2021-08-31 19:23:11 +0200

    use actual random values in dashboard

--- dashboard/src/random.ts
+++ dashboard/src/random.ts
@@ -1,8 +1,11 @@
 export function randomString(length: number): string {
+    const cryptoRandomValues = new Uint32Array(length);
+    window.crypto.getRandomValues(cryptoRandomValues);
+
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let str = '';
     for (let i = 0; i < length; i++) {
-        str += chars.charAt(Math.floor(Math.random() * chars.length));
+        str += chars.charAt(Math.floor(cryptoRandomValues[i] / 4294967295 * chars.length))
     }
     return str;
 }
