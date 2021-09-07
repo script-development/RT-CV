7c8fd02e5b8cd24aae7f9940af8b4f8e7a4be6a1
mjarkk <mkopenga@gmail.com>
2021-09-07 14:04:10 +0200

    remove redoc api docs page

--- dashboard/pages/docs.tsx
+++ dashboard/pages/docs.tsx
@@ -1,17 +1,38 @@
-import { RedocStandalone } from 'redoc';
+import { API } from "@stoplight/elements"
 import Header from '../components/header'
-import { secondaryColor } from '../src/theme'
+import { useEffect, useState } from "react"
+
+import '@stoplight/elements/styles.min.css'
 
 export default function Docs() {
+	const [currentSize, setCurrentSize] = useState(0)
+	const mobile = currentSize < 1000
+
+	useEffect(() => {
+		let lastSetCurrentSize = 0;
+		const onResize = () => {
+			// Only set the currentSize attr if the window size has changed by a certain amount
+			// This reduces the amount of unnecessary re-renders
+			const newSize = Math.round(window.innerWidth / 100) * 100
+			if (lastSetCurrentSize == newSize) return;
+
+			lastSetCurrentSize = newSize;
+			setCurrentSize(newSize)
+		}
+
+		window.addEventListener('resize', onResize)
+		return () => window.removeEventListener('resize', onResize)
+	}, [])
+
 	return (
-		<div className="root">
+		<div className="root" suppressHydrationWarning={true}>
 			<Header />
-			<RedocStandalone
-				specUrl="/api/v1/schema/openAPI"
-				options={{
-					theme: { colors: { primary: { main: secondaryColor } } },
-				}}
-			/>
+			{process.browser && <API
+				apiDescriptionUrl="/api/v1/schema/openAPI"
+				router="hash"
+				hideTryIt={true}
+				layout={mobile ? "stacked" : "sidebar"}
+			/>}
 			<style jsx>{`
 				.root {
 					background-color: white;
