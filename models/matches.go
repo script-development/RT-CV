71a1ea679dd0da24c2b98ab3c6593a6201d54768
mjarkk <mkopenga@gmail.com>
2021-09-09 11:21:36 +0200

    make BSON document fields equal to JSON fields
    This gives some consistency between the two formats.

--- models/matches.go
+++ models/matches.go
@@ -13,9 +13,9 @@ import (
 // We add omitempty to a lot of fields as it saves a lot of space in the database
 type Match struct {
 	db.M      `bson:",inline"`
-	RequestID primitive.ObjectID      `json:"requestId"`
-	ProfileID primitive.ObjectID      `json:"profileId"`
-	KeyID     primitive.ObjectID      `json:"keyId"`
+	RequestID primitive.ObjectID      `json:"requestId" bson:"requestId"`
+	ProfileID primitive.ObjectID      `json:"profileId" bson:"profileId"`
+	KeyID     primitive.ObjectID      `json:"keyId" bson:"keyId"`
 	When      jsonHelpers.RFC3339Nano `json:"when"`
 
 	Debug bool `bson:",omitempty" json:"debug"`
