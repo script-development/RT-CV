3ebf7edd221d093fdba17e330dcad15d2a429443
mjarkk <mkopenga@gmail.com>
2021-09-20 14:27:55 +0200

    add comment to next config

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -8,6 +8,7 @@ module.exports = {
         ]
     },
     webpack(config, _) {
+        // Required for monaco editor to work
         if (config.resolve.fallback) {
             config.resolve.fallback.fs = false
         }
