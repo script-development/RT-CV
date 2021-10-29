6d9f4b3979414f55a9d6683d7c4a18c496dcb76e
mjarkk <mkopenga@gmail.com>
2021-10-29 14:43:17 +0200

    Update docs for new authentication method

--- controller/schema_description.go
+++ controller/schema_description.go
@@ -22,7 +22,6 @@ Note that in the case of routes with multiple auth roles you only need one of th
 Notes:
 
 - The functions below don't exist they explain the kind of function that should be called
-- sha256 can also be used, use use replace sha512 with sha256 everywhere below
 - the ` + "`sha512`" + ` function should return a hex value not the bytes
 
 #### On app init
@@ -30,36 +29,6 @@ Notes:
 ` + mdBlockCodeSample("js", `
 apiKey = getApiKey();
 apiKeyID = getApiKeyID();
-seed = fetchJson("/api/v1/auth/seed").seed;
-salt = random(32);
-key = sha512(seed + apiKey + salt);
+key = "Basic " + apiKey + ":" + sha512(apiKeyID);
 `) + `
-
-#### For every request
-
-` + mdBlockCodeSample("js", `
-key = sha512(key + apiKey + salt);
-return (
-  "Authorization: Basic " + base64(`+"`sha512:${apiKeyID}:${salt}:${key}`"+`)
-);
-`) + `
-
-#### If auth fails while having a theoretically valid key
-
-1: just retry it (the server might be offline or whatever)
-
-` + mdBlockCodeSample("js", `
-return (
-  "Authorization: Basic " + base64(`+"`sha512:${apiKeyID}:${salt}:${key}`"+`)
-);
-`) + `
-
-2: Get a new salt and start over (basically going back to "on init")
-
-` + mdBlockCodeSample("js", `
-seed = fetchJson("/api/v1/auth/seed").seed;
-salt = random(32);
-key = sha512(apiKey + salt);
-key = sha512(key + apiKey + salt);
-return "Authorization: Basic " + base64(`+"`sha512:${apiKeyID}:${salt}:${key}`"+`);
-`)
+`
