9d5c42e2c714d497238c30b2f3bc89dcfbad9e15
mjarkk <mkopenga@gmail.com>
2022-07-07 11:47:26 +0200

    Remove matches from dashboard as it's not available anymore

--- dashboard/components/statistics.tsx
+++ dashboard/components/statistics.tsx
@@ -1,84 +1,16 @@
-import dynamic from 'next/dynamic'
 import { fetcher } from '../src/auth'
-import { formatRFC3339, subDays, startOfDay } from 'date-fns'
-import React, { useEffect, useMemo, useState } from 'react'
-import { Match } from '../src/types'
-import { BarChartProps } from './chartProps'
-import { CircularProgress, Tooltip } from '@mui/material'
-
-const BarChart = dynamic<BarChartProps>(() =>
-    import('./chart').then(m => m.BarChart),
-    {
-        ssr: false,
-        loading: () => <ChartLoader />,
-    },
-)
+import React, { useEffect, useState } from 'react'
+import { Tooltip } from '@mui/material'
 
 export default function Statistics() {
-    const [loading, setLoading] = useState(true)
-    const [matches, setMatches] = useState<Array<Match>>([])
-    const [dateRange, setDateRangeFromAndTo] = useState<[Date, Date] | undefined>(undefined)
     const [profilesStats, setProfilesStats] = useState({ total: undefined as undefined | number, usable: undefined as undefined | number })
 
-    const fetchAnalytics = async (from: Date, to: Date) => {
-        try {
-            setLoading(true)
-            const [fetchedData, profilesStats] = await Promise.all([
-                fetcher.fetch(`/api/v1/analytics/matches/period/${formatRFC3339(from)}/${formatRFC3339(to)}`),
-                fetcher.fetch(`/api/v1/profiles/count`),
-            ])
-            setProfilesStats(profilesStats)
-            setMatches(
-                fetchedData.map((entry: any) => ({
-                    ...entry,
-                    when: new Date(entry.when),
-                }))
-            )
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    const dayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Su'];
-
-    const matchesPerDayOfPrev7Days = useMemo(() => {
-        const resOffset = dateRange?.[1]?.getDay() || 0;
-
-        const res = matches
-            .reduce((acc, match) => {
-                acc[match.when.getDay()]++
-                return acc
-            }, [0, 0, 0, 0, 0, 0, 0])
-            .map((value, idx) => ({
-                label: dayNames[idx],
-                value,
-            }))
-
-        return [...res.splice(resOffset + 1), ...res]
-    }, [matches, dateRange])
-
     useEffect(() => {
-        const from = startOfDay(subDays(new Date(), 6));
-        const to = new Date();
-        setDateRangeFromAndTo([from, to])
-        fetchAnalytics(from, to)
+        fetcher.fetch(`/api/v1/profiles/count`).then(setProfilesStats)
     }, [])
 
     return (
         <div className="charts">
-            <div className="char">
-                <h3>Matches per day</h3>
-                <div className="box">
-                    {loading
-                        ? <ChartLoader />
-                        : <BarChart
-                            data={matchesPerDayOfPrev7Days}
-                            singleTooltip="match"
-                            multipleTooltip="matches"
-                        />
-                    }
-                </div>
-            </div>
             <div className="char">
                 <h3>Profiles Count</h3>
                 <div className="box">
@@ -113,24 +45,7 @@ export default function Statistics() {
                     overflow: hidden;
                     border-radius: 4px;
                     background-color: #424242;
-                    min-width: 300px;
-                }
-            `}</style>
-        </div>
-    )
-}
-
-function ChartLoader() {
-    return (
-        <div className="loader">
-            <CircularProgress />
-            <style jsx>{`
-                .loader {
-                    width: 285px;
-                    height: 200px;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
+                    width: 300px;
                 }
             `}</style>
         </div>
